795e8848d856fbc09b895d4d0cd5e28e2d7f01e3
aaramour@byui.edu <aaramour@byui.edu>
2024-04-26 13:14:39 -0400

    above and beyond?

--- W01/Controllers/lesson1.js
+++ W01/Controllers/lesson1.js
@@ -2,6 +2,11 @@ const aaronRoute =  (req, res) => {
     res.send('Aaron Eardley');
   };
 
+const maxRoute = (req, res) => {
+    res.send('Max Eardley');
+}
+
 module.exports = {
-    aaronRoute
+    aaronRoute,
+    maxRoute,
 };
